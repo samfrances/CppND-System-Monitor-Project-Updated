fa8aeb49e19b5935bac976d1f6b61c1751269023
Sam Frances <sam@samfrances.uk>
2019-10-25 16:58:10 +0100

    Add MockTurtle test example from https://github.com/google/googletest/blob/master/googlemock/docs/for_dummies.md
    This is just for reference

--- test/test_tests.cpp
+++ test/test_tests.cpp
@@ -1,5 +1,36 @@
-#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using ::testing::AtLeast;
+
+// TODO: Remove testing examples
 
 TEST(TestTestsWork, ShouldFail) {
     EXPECT_EQ(false, true);
+}
+
+class Turtle {
+  virtual void PenUp() = 0;
+  virtual void PenDown() = 0;
+  virtual void Forward(int distance) = 0;
+  virtual void Turn(int degrees) = 0;
+  virtual void GoTo(int x, int y) = 0;
+  virtual int GetX() const = 0;
+  virtual int GetY() const = 0;
+};
+
+class MockTurtle : public Turtle {
+ public:
+  MOCK_METHOD(void, PenUp, (), (override));
+  MOCK_METHOD(void, PenDown, (), (override));
+  MOCK_METHOD(void, Forward, (int distance), (override));
+  MOCK_METHOD(void, Turn, (int degrees), (override));
+  MOCK_METHOD(void, GoTo, (int x, int y), (override));
+  MOCK_METHOD(int, GetX, (), (const, override));
+  MOCK_METHOD(int, GetY, (), (const, override));
+};
+
+TEST(ExampleMock, MockWorks) {
+    MockTurtle turtle;
+    EXPECT_CALL(turtle, PenDown()).Times(AtLeast(1));
+    turtle.PenDown();
 }
