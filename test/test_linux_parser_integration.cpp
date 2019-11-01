394721710a855a96d80abdac5be5ea37419e2844
Sam Frances <sam@samfrances.uk>
2019-11-01 16:17:29 +0000

    Write filesystem integration test for LinuxParser::Pids()

--- test/test_linux_parser_integration.cpp
+++ test/test_linux_parser_integration.cpp
@@ -43,3 +43,29 @@ TEST(LinuxParserIntegrationTest, Kernel) {
     EXPECT_EQ(parser.Kernel(), "3.6.8-1.523");
   }
 }
+
+TEST(LinuxParserIntegrationTest, Pids) {
+  {
+    LinuxParser parser(GetTestFsRoot("one"));
+
+    auto actual_pids = parser.Pids();
+    std::sort(actual_pids.begin(), actual_pids.end());
+
+    std::vector<int> expected_pids{3, 83, 490, 1259, 8193};
+    std::sort(expected_pids.begin(), expected_pids.end());
+
+    EXPECT_THAT(actual_pids, ::testing::ContainerEq(expected_pids));
+  }
+
+  {
+    LinuxParser parser(GetTestFsRoot("two"));
+
+    auto actual_pids = parser.Pids();
+    std::sort(actual_pids.begin(), actual_pids.end());
+
+    std::vector<int> expected_pids{928, 9, 4482, 8840, 820, 3, 5, 7, 93928};
+    std::sort(expected_pids.begin(), expected_pids.end());
+
+    EXPECT_THAT(actual_pids, ::testing::ContainerEq(expected_pids));
+  }
+}
